9b188a1c7d75668d42edb1215920126de787f908
Aaron Anderson <aaronanderson@acm.org>
2019-09-03 21:51:25 -0500

    init example

---
+++ example/src/test/java/com/github/quarkus/NativeExampleResourceIT.java
@@ -0,0 +1,9 @@
+package com.github.quarkus;
+
+import io.quarkus.test.junit.SubstrateTest;
+
+@SubstrateTest
+public class NativeExampleResourceIT extends ExampleResourceTest {
+
+    // Execute the same tests but in native mode.
+}
